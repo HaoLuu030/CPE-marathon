68c71f1c010b9e7c416753dd5a1b36f51e6e3fef
HaoLuu030 <110086916+HaoLuu030@users.noreply.github.com>
2025-04-26 23:19:37 +0800

    Update Solution.cpp

--- 29-04-2025/Hao/Solution.cpp
+++ 29-04-2025/Hao/Solution.cpp
@@ -1,1 +1,58 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+// make shift function for gcd
+int gcd (int a, int b)
+{
+    if (a % b == 0) return b;
+    else return gcd(b, a % b);
+}
+
+// make shift function for power
+int pow(int b, int e)
+{
+    if (e == 0) return 1;
+    else return b * pow(b, e - 1);
+}
+
+// we can either do this
+// or we can use stoi with specialized
+// paramter to convert the string to
+// binary directly
+int string_to_decimal(string s)
+{
+    int sum = 0;
+    // reverse the string
+    // so the summing direction
+    // aligns with way we convert a binary
+    // to a decimal number
+    for (int i = 0; i < s.size() / 2; i++)
+    {
+        char temp = s[i];
+        s[i] = s[s.size() - 1 - i];
+        s[s.size() - 1 - i] = temp;
+    }
+    // calculate sum
+    for (int i = 0; i < s.size(); i++)
+    {
+        sum += pow(2, i) * (s[i] - '0');
+    }
+    return sum;
+}
+
+
+
+int main()
+{
+    int N;
+    string S1, S2;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> S1 >> S2;
+        int L = gcd(string_to_decimal(S1), string_to_decimal(S2));
+        cout << "Pair: " << i + 1 << ": " << ((L > 1) ? "All you need is love!" : "Love is not all you need!") << endl;
+    }
+}

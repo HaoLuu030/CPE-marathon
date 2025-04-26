3f86e44c62cb42edbc149c67cc54653f836ebf98
HaoLuu030 <110086916+HaoLuu030@users.noreply.github.com>
2025-04-26 10:43:50 +0800

    Update Solution.cpp

--- 26-04-2025/Hao/Solution.cpp
+++ 26-04-2025/Hao/Solution.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
 
+using namespace std;
+
+// function for gcd
+int gcd(int a, int b)
+{
+    int remainder = a % b;
+    if (remainder == 0) return b;
+    else return gcd(b, remainder);
+}   
+
+int main()
+{
+    int N = 0;
+    int G = 0;
+    // only process the number if N != 0
+    if (N != 0)
+    {
+        for (int i = 1; i < N; i++)
+        {
+            for (int j = i + 1; j <= N; j++)
+            {
+                G += gcd(i, j);
+            }
+        }
+        cout << G << endl;
+    }
+}

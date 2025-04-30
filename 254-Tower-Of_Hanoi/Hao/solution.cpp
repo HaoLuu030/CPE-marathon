3b647c244497f4a54884c9ae97c3c2ff517802fd
Hao Luu <luuquachgiahao99@gmail.com>
2025-04-30 21:12:59 +0800

    add solution for tower-of-hanoi problem

---
+++ 254-Tower-Of_Hanoi/Hao/solution.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+// global variable to track move and number of disks on each rod
+int move__ = 0;
+int rods[3] = {0};
+
+// for more details about the algorithm of the tower of hanoi problem
+// please refer to the algorithm cpp file in the parent directory
+void towerOfHanoi(int n, int start, int end, int terminate_move) {
+  // terminate when the number of moves is reached
+  if (move__ >= terminate_move)
+    return;
+  if (n == 1) {
+    move__++;
+    rods[start - 1]--;
+    rods[end - 1]++;
+    return;
+  }
+  int auxiliary = 6 - start - end;
+  towerOfHanoi(n - 1, start, auxiliary, terminate_move);
+  if (move__ >= terminate_move)
+    return;
+  move__++;
+  rods[start - 1]--;
+  rods[end - 1]++;
+  towerOfHanoi(n - 1, auxiliary, end, terminate_move);
+}
+
+int main() {
+    int a, b;
+    while(cin >> a >> b)
+    {
+        if (a == 0 && b == 0) break;
+        for (int i = 0; i < 3; i++)
+        {
+            rods[i] = 0;
+        }
+        move__ = 0;
+        rods[0] = a;
+        string output = "";
+        towerOfHanoi(a, 1, 3, b);
+        for (int i = 0; i < 3; i++)
+        {
+            output += to_string(rods[i]);
+            if (i != 2) output+= ' ';
+        }
+        cout << output << '\n';
+    }
+}

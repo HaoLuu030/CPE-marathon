30bdec5ae969060d39b223fdb6a3f15f53146128
yennhi1304 <yenhi130406@gmail.com>
2025-05-01 21:06:41 +0800

    Update solution.cpp

--- 374-Big-Mod/Nhi/solution.cpp
+++ 374-Big-Mod/Nhi/solution.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+long long bigMod(int B, int P, int M)
+{
+    if (P == 0) return 1;
+    long long half = bigMod(B, P / 2, M);
+    if (P % 2 == 0)
+    {
+        half = (half*half) % M;
+    }
+    else
+    {
+        half = B * half * half % M;
+    }
+    return half;
+}
+
+int main()
+{
+    long long B, P, M;
+    while (cin >> B >> P >> M)
+    {
+        cout << bigMod(B, P, M) << endl;
+    }
+    return 0;
+}

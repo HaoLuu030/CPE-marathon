994d97a25d48d48faa4c6a568f1c8e9f3fcd8f69
Hao Luu <luuquachgiahao99@gmail.com>
2025-05-17 16:57:37 +0800

    added basic comments for euclid exntended problem

--- 10104-Euclid-Extended/Hao/solution.cpp
+++ 10104-Euclid-Extended/Hao/solution.cpp
@@ -2,17 +2,21 @@
 
 using namespace std;
 
+// keep x and y as the global variable for modification
 int x, y;
 int extendedEuclid(int a, int b, int& x, int& y)
 {
+    // base case
     if (b == 0)
     {
         x = 1;
         y = 0;
         return a;
     }
+    // create x1 and y1 for recursive calls
     int x1, y1;
     int gcd = extendedEuclid(b, a % b, x1, y1);
+    // this can be proven using the basic version of euclid algorithm
     x = y1;
     y = x1 - (a / b) * y1;
     return gcd;
